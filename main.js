528d745e139064a6540cd559a70f195ad00972fe
tumijiu <nahuel.retamoso@gmail.com>
2022-08-24 23:15:09 -0300

    commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
+const websocket = require('websocket')
 
 const ProductosApi = require('./api.js')
 
@@ -10,28 +11,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-//Set engine
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
 
 
 
-//--------------------------------------------
+
 
 app.post('/productos', (req, res) => {
-    // handlebars - render
+
     const producto = productosApi.guardar(req.body)
     res.render('formulario', { producto })
 })
 
 app.get('/productos', (req, res) => {
-    // handlebars - render
+
     const productos = productosApi.listarAll()
     res.render('vista', { productos })
 });
 
-//--------------------------------------------
+
 const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
